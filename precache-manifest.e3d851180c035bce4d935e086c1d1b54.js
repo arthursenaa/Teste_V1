4f7794866371477741d508a1e9c1201fc31d2711
asassena1002@hotmail.com <asassena1002@hotmail.com>
2020-02-27 16:28:01 -0300

    Updates

---
+++ precache-manifest.e3d851180c035bce4d935e086c1d1b54.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f02abda11991400ed09e072723bf07f",
+    "url": "/projeto/index.html"
+  },
+  {
+    "revision": "660ecf9e5281509c2b8b",
+    "url": "/projeto/static/css/2.558afbfe.chunk.css"
+  },
+  {
+    "revision": "6af86becca674ae45d17",
+    "url": "/projeto/static/css/main.57992efe.chunk.css"
+  },
+  {
+    "revision": "660ecf9e5281509c2b8b",
+    "url": "/projeto/static/js/2.06bfabf2.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/projeto/static/js/2.06bfabf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af86becca674ae45d17",
+    "url": "/projeto/static/js/main.7e93e0b0.chunk.js"
+  },
+  {
+    "revision": "bd5cddd6f761dfc661a4",
+    "url": "/projeto/static/js/runtime-main.9a45c017.js"
+  },
+  {
+    "revision": "a99f861bcdc66f0ba36f2ef4aef5a1c2",
+    "url": "/projeto/static/media/Dev.a99f861b.png"
+  },
+  {
+    "revision": "498ab58c63907a6068e108e0232a86ca",
+    "url": "/projeto/static/media/LogoSenai.498ab58c.jpg"
+  },
+  {
+    "revision": "833bbfcb2c1a840edfa2b04b19c7ce14",
+    "url": "/projeto/static/media/code.833bbfcb.jpg"
+  },
+  {
+    "revision": "8f84ed0408195d5a282b4973790f6da5",
+    "url": "/projeto/static/media/laptop.8f84ed04.png"
+  },
+  {
+    "revision": "38fa79f122a045254dd4ad62b8edbf74",
+    "url": "/projeto/static/media/login.38fa79f1.JPG"
+  },
+  {
+    "revision": "9b28be986687646a86c81cf6d2d0a86f",
+    "url": "/projeto/static/media/mult.9b28be98.png"
+  },
+  {
+    "revision": "2670f80ca217446ef2b19fb62110170e",
+    "url": "/projeto/static/media/multimidia.2670f80c.jpg"
+  },
+  {
+    "revision": "8ba229755b7ed4dfd14148ae625debb6",
+    "url": "/projeto/static/media/redes.8ba22975.jpg"
+  },
+  {
+    "revision": "00272fdedd7c0a9822fcc52210292090",
+    "url": "/projeto/static/media/usuarios.00272fde.png"
+  }
+]);
